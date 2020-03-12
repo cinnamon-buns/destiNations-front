5b5876db4d7f7efe176b716f4b77ec803fa0135a
Ashley Kekona <56838258+akekona8@users.noreply.github.com>
2020-03-12 18:11:57 +0900

    set up firebase database

---
+++ Assets/Scripts/FirebaseSaveData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using Proyecto26;
+
+public class FirebaseSaveData : MonoBehaviour
+{
+    // DatabaseReference reference;
+    public Text text;
+    public InputField inputField;
+
+    public static string countryAdded;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     // Set up the Editor before calling into the realtime database.
+    //     FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://destinations-a0b80.firebaseio.com/");
+
+    //     // Get the root reference location of the database.
+    //     reference = FirebaseDatabase.DefaultInstance.RootReference;
+    // }
+
+    // Update is called once per frame
+    public void OnSubmit()
+    {
+        Debug.Log("submit!");
+        countryAdded = inputField.text;
+        PostToDatabase();
+    }
+
+    private void PostToDatabase()
+    {
+        Debug.Log("posting to database");
+        User user = new User();
+        Debug.Log(user);
+        RestClient.Post("https://destinations-a0b80.firebaseio.com/.json", user);
+        Debug.Log("after calling restClient");
+    }
+}
---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class User
+{
+    public string userName;
+    public User()
+    {
+        userName = FirebaseSaveData.countryAdded;
+        Debug.Log(userName);
+    }
+}
